b42a06dc6c423c7902989a7afe1127c5d8863985
Edinnnnnnn <edinmacanids1@gmail.com>
2023-04-15 15:27:42 +0200

    del

--- movie/database/deleteUsers.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-$sql = "DELETE FROM users WHERE id=:id";
-$prep = $con->prepare($sql);
-$prep->prepare(':id',$id);
-$prep->execute();
-
-header("Location: dashboard.php");
-?>
